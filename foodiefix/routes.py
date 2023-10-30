1c5e81eb823662094a3eff39901dc2da5d6068e0
Renz2299 <karenza.kv@outlook.com>
2023-10-30 18:18:43 +0000

    add recipe form created, post method yet to work, need to figure out whats wrong in created at & created by fields

--- foodiefix/routes.py
+++ foodiefix/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from foodiefix import app, db
 from foodiefix.models import User, Recipe
 
@@ -6,3 +6,24 @@ from foodiefix.models import User, Recipe
 @app.route("/")
 def home():
     return render_template("recipes.html")
+
+
+@app.route("/my_recipes")
+def my_recipes():
+    return render_template("my_recipes.html")
+
+
+@app.route("/add_recipe", methods=["GET", "POST"])
+def add_recipe():
+    if request.method == "POST":
+        recipe = Recipe(
+            recipe_title=request.form.get("recipe_title"),
+            recipe_description=request.form.get("recipe_description"),
+            recipe_ingredients=request.form.get("recipe_ingredients"),
+            recipe_method=request.form.get("recipe_method"),
+            recipe_photo=request.form.get("recipe_photo")
+        )
+        db.session.add(recipe)
+        db.session.commit()
+        return redirect(url_for("my_recipes"))
+    return render_template("add_recipe.html")
